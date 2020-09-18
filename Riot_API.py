527054fc71144c0b52af9c6c939480b07ade39b2
liujunhan <jimliujunhan@sina.cn>
2020-09-17 23:40:23 -0400

    update

--- Riot_API.py
+++ Riot_API.py
@@ -83,7 +83,7 @@ class Riot:
     def get_league_entry(self):
         start_page, end_page = 1, 100
         # configure tier and divisions of summoners
-        tier, division = self.rank_tiers[2], self.division[1]
+        tier, division = self.rank_tiers[2], self.division[2]
         print(">>> Now extracting summoner information of [ {} {} ]".format(tier, division))
         while start_page < end_page:
             # request summoner information from RIOT
@@ -107,7 +107,8 @@ class Riot:
 
         print("Work finished, stop requesting from RIOT.")
 
-
+    def MATCH_V4(self):
+        return
 
 if __name__ == "__main__":
     riot = Riot(access_key=ACCESS_KEY)
