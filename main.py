31a321a50676dd685696daae43d8ebedf615927a
liujunhan <jimliujunhan@sina.cn>
2020-09-26 21:20:01 -0400

    update

--- main.py
+++ main.py
@@ -42,6 +42,28 @@ class main:
         data = [tuple(x) for x in df.to_numpy()]
         utils.populate_db(sql, data)
 
+        # sql = """
+        #     insert ignore into RIOT.match_list
+        #     ("gameId","gameCreation","gameDuration",
+        #     "team1_win","team1_firstBlood","team1_firstTower",
+        #     "team1_firstInhibitor","team1_firstBaron","team1_firstDragon","team1_firstRiftHerald",
+        #     "team1_towerKills","team1_inhibitorKills","team1_baronKills","team1_dragonKills","team1_vilemawKills",
+        #     "team1_riftHeraldKills","team1_dominionVictoryScore",
+        #     "team1_ban1","team1_ban2","team1_ban3","team1_ban4","team1_ban5",
+        #     "team1_champ1_championId","team1_champ2_championId","team1_champ3_championId",
+        #     "team1_champ4_championId","team1_champ5_championId","team1_champ1_statId","team1_champ2_statId",
+        #     "team1_champ3_statId","team1_champ4_statId","team1_champ5_statId","team2_win","team2_firstBlood",
+        #     "team2_firstTower","team2_firstInhibitor","team2_firstBaron","team2_firstDragon",
+        #     "team2_firstRiftHerald","team2_towerKills","team2_inhibitorKills","team2_baronKills","team2_dragonKills",
+        #     "team2_vilemawKills","team2_riftHeraldKills","team2_dominionVictoryScore","team2_ban1","team2_ban2","team2_ban3",
+        #     "team2_ban4","team2_ban5","team2_champ1_championId","team2_champ2_championId","team2_champ3_championId",
+        #     "team2_champ4_championId","team2_champ5_championId","team2_champ1_statId","team2_champ2_statId","team2_champ3_statId",
+        #     "team2_champ4_statId","team2_champ5_statId")
+        #     values
+        #     (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s
+        #     ,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
+        # """
+
 
 
 
